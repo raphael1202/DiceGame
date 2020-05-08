87a5e33f43a9cc57d8128316fe60fd9c0590289d
Raphael Duarte Sena <raphael_duarte@outlook.com>
2020-05-08 10:34:31 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-var randomNumber1  = Math.floor(Math.random() * 6) + 1;
-var img = "images/dice" + randomNumber1 + ".png";
-var randomNumber2  = Math.floor(Math.random() * 6) + 1;
-var img2 = "images/dice" + randomNumber2 + ".png";
 
-document.querySelectorAll("img")[0].setAttribute("src", img);
+function rollDices(){
+    var randomNumber1  = Math.floor(Math.random() * 6) + 1;
+    var img = "images/dice" + randomNumber1 + ".png";
+    var randomNumber2  = Math.floor(Math.random() * 6) + 1;
+    var img2 = "images/dice" + randomNumber2 + ".png";
 
-document.querySelectorAll("img")[1].setAttribute("src", img2);
+    document.querySelectorAll("img")[0].setAttribute("src", img);
 
-WhoWins(randomNumber1, randomNumber2);
+    document.querySelectorAll("img")[1].setAttribute("src", img2);
 
+    WhoWins(randomNumber1, randomNumber2);
+}
 
 function WhoWins(random1, random2){
     if(random1 > random2)
